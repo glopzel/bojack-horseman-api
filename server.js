b6cce33a0627f5f8e7724f7a4da2bb7321178a9e
glopzel <74939915+lozzel@users.noreply.github.com>
2022-06-22 13:16:06 -0400

    made db variables be global

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 let connectionStr = process.env.DB_STRING;
+let db, infoCollection, dbName = 'bojack-api'
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
@@ -19,32 +20,34 @@ const titleCase = (name) => {
 
 app.use(express.static(__dirname + '/public'))
 
+
 MongoClient.connect(connectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log('connected to database');
         
-        const db = client.db('bojack-api');
+        db = client.db(dbName);
         
-        const infoCollection = db.collection('bojack-info');
-
-    app.get('/', (request, response) => {
-        response.sendFile(__dirname + '/index.html')
-    });
+        infoCollection = db.collection('bojack-info'); 
         
-    app.get('/api/characters/:name', (request, response) => {
-        const characterName = request.params.name;
-        const fancyName = titleCase(characterName);
-
-        infoCollection.find({"name": fancyName}).toArray()
-            .then(results => {
-                response.json(results[0]);
-            })
-            .catch(error => {
-                console.error(error);
-            })
-    });
 })
 .catch(error => console.error(error));
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/characters/:name', (request, response) => {
+    const characterName = request.params.name;
+    const fancyName = titleCase(characterName);
+
+    infoCollection.find({"name": fancyName}).toArray()
+        .then(results => {
+            response.json(results[0]);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server running port localhost:${PORT}, go catch it!`);
