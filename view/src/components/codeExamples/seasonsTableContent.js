44943be6ffbd85371cef042cc2d9c92eb0c9715f
Angela Lopez <angelalopez96@gmail.com>
2023-09-24 16:57:16 -0300

    html supremacy

--- view/src/components/codeExamples/seasonsTableContent.js
+++
@@ -1,7 +0,0 @@
-let seasonsTableContent = [
-    ['season', 'String', 'Number of season'],
-    ['year', 'String', 'Year of release'],
-    ['episodes', 'Array', 'List of episodes']
-]
-
-module.exports = seasonsTableContent
