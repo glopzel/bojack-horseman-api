0573c0e85042f65051455192a9323ae1ea9b4fea
Angela Lopez <angelalopez96@gmail.com>
2024-03-21 16:43:11 -0300

    reorder endpointss, delete commented code

--- server.js
+++ server.js
@@ -14,16 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-// app.use(express.static(path.resolve(__dirname, './view/build')));
-// app.use(express.static(path.join(__dirname, './view/build')));
-
-// app.use(express.static('./static'))
 app.use(express.static(path.join(__dirname, 'view/dist')));
-
-app.get('*', (req, res) => {
-    const pathToView = path.join(__dirname, './view/dist/', 'index.html')
-    res.sendFile(pathToView)
-})
 
 const fetching = async (table, filterColumn, param) => {
     // table as in endpoints
@@ -98,8 +89,9 @@ app.get("/api/seasons/:num", async (req, res) => {
     res.send(data)
 })
 
-app.get('/', (req, res) => {
-   res.sendFile('./static/index.html')
+app.get('*', (req, res) => {
+    const pathToView = path.join(__dirname, './view/dist/', 'index.html')
+    res.sendFile(pathToView)
 })
 
 app.listen(process.env.PORT || PORT, () => {
